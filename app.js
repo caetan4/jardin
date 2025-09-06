98bd735c33e75663bb293445a695df13bfd53917
caetan4 <cprieto@centro.edu.mx>
2025-09-05 23:34:50 -0600

    Agrege canvas y hojas intente poner el tronco

---
+++ app.js
@@ -0,0 +1,70 @@
+console.log("Sesion 5. Ejercicio02: Geometrias");
+console.log("THREE: ", THREE);
+
+// 1. Definir nuestro canvas
+const canvas = document.getElementById("lienzo")
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// 4. Creamos nuestros elementos básicos
+// Escena, escenario, Mesh, Camras
+//escena
+const scene = new THREE.Scene();
+
+//camara
+const camera = new THREE.PerspectiveCamera(45, canvas.width/canvas.height, 0.1, 1000);
+
+//mesh
+/////Geometrias
+const geometry = new THREE.SphereGeometry(1,18,18);
+const geometr = new THREE.SphereGeometry(1,18,18);
+const geomet = new THREE.SphereGeometry(1,18,18);
+const CylinderGeometryr= new THREE.CylinderGeometry( 5, 5, 20, 32 ); 
+const CylinderMaterial= new THREE.MeshBasicMaterial( {color: "#5a341aff"} ); 
+const cylinder = new THREE.Mesh( CylinderGeometryr, CylinderMaterial); scene.add( cylinder );
+//const geomet = new THREE.SphereGeometry(1,18,18);
+const material = new THREE.MeshPhongMaterial({
+  flatShading: true, specular : "#ffffffff", shininess: 100, color: "rgba(118, 141, 15, 1)"});
+
+const mesh = new THREE.Mesh(geometry, material);
+const mesh2 = new THREE.Mesh(geometr, material);
+const mesh3 = new THREE.Mesh(geomet, material);
+
+scene.add(mesh);
+scene.add(mesh2);
+scene.add(mesh3);
+scene.add(cylinder);
+mesh.position.z = -8;
+mesh2.position.z = -8;
+mesh.position.x = -1;
+mesh2.position.x = 1;
+mesh3.position.x = 1;
+mesh3.position.y = 4;
+mesh3.position.z = 4;
+
+
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(canvas.width, canvas.height);
+
+//Dar instrucciones de renderizar o imprimir nuestro primer frame
+renderer.render(scene, camera);
+
+
+// Tip para animar nuestro mesh:
+function animate() {
+    requestAnimationFrame(animate);
+
+
+ renderer.render(scene, camera);
+ }
+ animate();
+
+ const topLight = new THREE.PointLight("#506b04ff", 100, 100);
+topLight.position.y = 5;
+scene.add(topLight);
+
+const frontLight = new THREE.PointLight("#0000ff", 10, 100);
+frontLight.position.set(3,1,3);
+scene.add(frontLight);
