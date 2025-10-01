56b17668901a9ca79118840d48dbf88de6167bb8
caetan4 <cprieto@centro.edu.mx>
2025-09-30 19:52:29 -0600

    mantener figura dentro del lienzo

--- 08 Elusive Square/app.js
+++ 08 Elusive Square/app.js
@@ -3,17 +3,18 @@ console.log("Triángulo con GSAP");
 const figura = document.getElementById("figura");
 
 window.addEventListener("click", () => {
-  const figuraWidth = 80;
-  const figuraHeight = 70;
+  const figuraWidth = 80;  
+  const figuraHeight = 70; 
 
   const maxX = window.innerWidth - figuraWidth;
   const maxY = window.innerHeight - figuraHeight;
 
+  // Coordenadas aleatorias dentro del área segura
   const randomX = Math.random() * maxX;
   const randomY = Math.random() * maxY;
 
   gsap.to(figura, {
-    duration: 1.5,
+    duration: 1.2,
     x: randomX - window.innerWidth / 2,
     y: randomY - window.innerHeight / 2,
     ease: "power2.inOut"
